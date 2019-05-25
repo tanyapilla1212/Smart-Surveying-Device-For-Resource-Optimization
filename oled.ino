9c9ac8e9f341f65c159282a546c6f33ed8bba6cf
tanyapilla1212 <50827988+tanyapilla1212@users.noreply.github.com>
2019-05-25 13:12:36 +0530

    Add files via upload

---
+++ oled.ino
@@ -0,0 +1,93 @@
+#include <ESP8266WiFi.h>
+
+
+const char* ssid     = "chi";
+const char* password = "navyanavya";
+
+const char* host = "https://sip-iot-app1.eu-gb.mybluemix.net";
+#include <Wire.h>
+#include <Adafruit_SSD1306.h>
+#include <Adafruit_GFX.h>
+#define SSD1306_LCDHEIGHT 64
+
+// OLED display TWI address
+#define OLED_ADDR   0x3C
+
+Adafruit_SSD1306 display(-1);
+
+#if (SSD1306_LCDHEIGHT != 64)
+#error("Height incorrect, please fix Adafruit_SSD1306.h!");
+#endif
+
+void setup() {
+  Serial.begin(115200);
+  delay(10);
+  
+
+  // We start by connecting to a WiFi network
+
+  Serial.println();
+  Serial.println();
+  Serial.print("Connecting to ");
+  Serial.println(ssid);
+  
+  WiFi.begin(ssid, password);
+  
+  while (WiFi.status() != WL_CONNECTED) {
+    delay(500);
+    Serial.print(".");
+  }
+
+  Serial.println("");
+  Serial.println("WiFi connected");  
+  Serial.println("IP address: ");
+  Serial.println(WiFi.localIP());
+
+
+}
+void loop() {
+ 
+  Serial.print("connecting to ");
+  Serial.println(host);
+  
+  // Use WiFiClient class to create TCP connections
+  WiFiClient client;
+  const int httpPort = 80;
+  if (!client.connect(host, httpPort)) {
+    Serial.println("connection failed");
+    return;
+  }
+  
+  // We now create a URI for the request
+  String url ="1";
+ 
+  Serial.print("Requesting URL: ");
+  Serial.println(url);
+  
+  // This will send the request to the server
+  client.print(String("GET ") + url + " HTTP/1.1\r\n" +
+               "Host: " + host + "\r\n" + 
+               "Connection: close\r\n\r\n");
+  delay(10);
+  
+  // Read all the lines of the reply from server and print them to Serial
+  while(client.available()){
+    String line = client.readStringUntil('\r');
+    Serial.print("line");
+    Serial.print(line);
+  }
+  delay(1000);
+  Serial.println();
+  Serial.println("closing connection");
+  delay(4000);
+
+  display.begin(SSD1306_SWITCHCAPVCC, OLED_ADDR);
+  display.clearDisplay();
+// display a line of text
+  display.setTextSize(1);
+  display.setTextColor(BLACK,WHITE);
+  display.setCursor(0,10);
+  display.print(url);
+  // update display with all of the above graphics
+  display.display(); 
+}
